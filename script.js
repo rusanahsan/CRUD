2fd82d19de426b766bca87a5e25b80f828cf96c4
rusanahsan <86840459+rusanahsan@users.noreply.github.com>
2021-09-25 23:30:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+var db=document.getElementById("db");
+var table={};
+var enable=true;
+var currentId="";
+function deleteRow(obj){
+    if(enable){
+        let ind=obj.parentNode.parentNode.rowIndex;
+        db.deleteRow(ind);
+        delete table[db.rows[ind].cells[0].textContent];
+    }
+}
+function deleteRows(obj){
+    db.deleteRow(obj.parentNode.parentNode.rowIndex);
+    enable=true;
+}
+function editRow(obj){
+    if(enable){
+        enable=false;
+        let arr=db.rows[obj.parentNode.parentNode.rowIndex].cells;
+        currentId=arr[0].textContent;
+        arr[0].innerHTML=`<input type="text" placeholder="Student Id">`;
+        arr[1].innerHTML=`<input type="text" placeholder="Name">`;
+        arr[2].innerHTML=`<input type="date" placeholder="DOB">`;
+        arr[3].innerHTML=`<input type="text" placeholder="Address">`;
+        arr[4].textContent="N/A";
+        arr[5].innerHTML=`<button type="button" class="save" onclick="saveRow(this)">Save</button>
+        <button type="button" class="cancel" onclick="cancelRow(this)">Cancel</button>`;
+    }
+}
+function cancelRow(obj){
+    let rownum=obj.parentNode.parentNode.rowIndex;
+    db.rows[rownum].innerHTML=table[currentId];
+    enable=true;
+}
+function saveRow(obj){
+    let rownum=obj.parentNode.parentNode.rowIndex;
+    let arr=db.rows[rownum].cells;
+    if(arr[0].firstChild.value==""){
+        alert('Please enter student id');
+        return;
+    }
+    else if(typeof table[arr[0].firstChild.value]!='undefined' && arr[0].firstChild.value!=currentId){
+        alert('Please enter unique student id');
+        return;
+    }
+    else if(arr[1].firstChild.value==""){
+        alert('Please enter your name');
+        return;
+    }
+    else if(arr[2].firstChild.value==""){
+        alert('Please enter your DOB');
+        return;
+    }
+    else if(arr[3].firstChild.value==""){
+        alert('Please enter your address');
+        return;
+    }
+    else{
+        let d=new Date();
+        arr[0].innerHTML=arr[0].firstChild.value;
+        arr[1].innerHTML=arr[1].firstChild.value;
+        let val=arr[2].firstChild.value.split('-');
+        arr[2].innerHTML=val[2]+"/"+val[1]+"/"+val[0];
+        arr[3].innerHTML=arr[3].firstChild.value;
+        var dd=String(d.getDate()).padStart(2,'0');
+        var mm=String(d.getMonth()+1).padStart(2,'0');
+        var yyyy=d.getFullYear();
+        arr[4].textContent=dd+"/"+mm+"/"+yyyy+" "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+        arr[5].innerHTML=`<button type="button" class="edit" onclick="editRow(this)">Edit</button>
+        <button type="button" class="delete" onclick="deleteRow(this)">Delete</button>`;
+        enable=true;
+    }
+}
+function addRow(){
+    if(!enable)
+        return;
+    enable=false;
+    currentId="";
+    let tabbod=document.getElementById("tabbod");
+    tabbod.innerHTML+=`<tr><td><input type="text" placeholder="Student Id"></td>
+    <td><input type="text" placeholder="Name"></td><td><input type="date" placeholder="DOB"></td>
+    <td><input type="text" placeholder="Address"></td><td>N/A</td>
+    <td><button type="button" class="save" onclick="saveRow(this)">Save</button>
+    <button type="button" class="cancel" onclick="deleteRows(this)">Cancel</button></td>`;
+}
+function populateTable(){
+    console.log(db.rows.length);
+    for(let i=1;i<db.rows.length;i++){
+        let key=db.rows[i].cells[0].textContent;
+        table[key]=db.rows[i].innerHTML;
+    }
+}
+function deleteallRow(){
+    if(enable){
+        table={};
+        document.getElementById("tabbod").innerHTML="";
+    }
+}
+populateTable();
